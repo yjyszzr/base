69006649cf653ff45a04182906a049402c5df83a
yjyszzr <1053195207@qq.com>
2018-05-08 22:51:27 +0800

    modified
    Change-Id: Ifc1c11bad979383ae99d17e0b151755a30de7549
    Signed-off-by: yjyszzr <1053195207@qq.com>

--- src/main/java/com/dl/base/constant/CommonConstants.java
+++ src/main/java/com/dl/base/constant/CommonConstants.java
@@ -30,4 +30,29 @@ public class CommonConstants {
     
     public static final String HTTP_HEADER_DEVICE = "dl-device";
     public static final String CONTEXT_KEY_USER_DEVICE = "currentUserDevice";
+    
+    //消息模板
+    public static final String FORMAT_REWARD_TITLE = "中奖啦";
+    public static final String FORMAT_REWARD_DESC = "恭喜您购买的{0}中了{1}元";
+    
+    public static final String FORMAT_PRINTLOTTERY_TITLE = "出票失败";
+    public static final String FORMAT_PRINTLOTTERY_DESC = "抱歉，您购买的{0}出票失败，请重新购买";
+    
+    public static final String FORMAT_BONUS_TITLE = "优惠券到期通知";
+    public static final String FORMAT_BONUS_DESC = "您有优惠券即将到期，请尽快使用";
+    
+//    1、中奖提示：其中“竞彩足球”是彩种，“9”是中奖金额，这2个字段由服务端控制
+//    标题＝中奖啦
+//    描述＝恭喜您购买的【竞彩足球】中了9元
+//    推送时间＝实时推送
+//
+//    2、出票失败提示：其中“竞彩足球”是彩种，这个字段由服务端控制
+//    标题＝出票失败
+//    描述＝抱歉，您购买的【竞彩足球】出票失败，请重新购买
+//    推送时间＝实时推送
+//
+//    3、优惠券到期提示：
+//    标题＝优惠券到期通知
+//    描述＝您有优惠券即将到期，请尽快使用
+//    推送时间＝到期日前1天晚上7点推送
 }
