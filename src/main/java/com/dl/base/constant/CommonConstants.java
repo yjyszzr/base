92222de909e9e865055db22a9dbe7963c138e518
duanyu <duanyu@caixiaomi.com>
2018-06-15 11:37:27 +0800

    update

--- src/main/java/com/dl/base/constant/CommonConstants.java
+++ src/main/java/com/dl/base/constant/CommonConstants.java
@@ -52,7 +52,7 @@ public class CommonConstants {
     public static final String FORMAT_WITHDRAW_CONTENT = "提现{0}元";
     public static final String FORMAT_WITHDRAW_SUC_CONTENT_DESC = "提现成功";
     public static final String FORMAT_WITHDRAW_FAIL_CONTENT_DESC = "提现成功";
-    public static final String FORMAT_WITHDRAW_MSG_DESC = "申请时间：{0}#审核通过时间：{1}元#提现成功时间：{2}";
+    public static final String FORMAT_WITHDRAW_MSG_DESC = "申请时间：{0}#审核通过时间：{1}#提现成功时间：{2}";
     
     public static final String FORMAT_PRINTLOTTERY_PUSH_TITLE = "出票失败";
     public static final String FORMAT_PRINTLOTTERY_PUSH_DESC = "抱歉，您购买的【竞彩足球】出票失败，请重新购买";
