962e977c30dc2f6f5ff9a99f455ee8da4001f1d5
yjyszzr <1053195207@qq.com>
2018-07-16 15:31:01 +0800

    modified
    Change-Id: Ic0174ca2345bf8b56bbfb8cd2c189711508b5208
    Signed-off-by: yjyszzr <1053195207@qq.com>

--- src/main/java/com/dl/base/util/StringHelper.java
+++ src/main/java/com/dl/base/util/StringHelper.java
@@ -7,4 +7,12 @@ public class StringHelper {
     public static String getObjectValue(Object obj){
         return obj==null?"":obj.toString();
     }
+    
+    public static String replaceChangeStr(String oldStr) {
+    	if(oldStr.contains("↓") || oldStr.contains("↑")) {
+    		return oldStr.substring(0, oldStr.length() - 1);
+    	}
+    	return oldStr;
+    }
+    
 }
