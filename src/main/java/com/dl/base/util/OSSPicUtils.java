05d4d2a3a4637f24a9aa048399190c8400917818
wenhuaiteng <wenhuaiteng@126.com>
2018-11-19 11:01:39 +0800

    OSS图片处理相关...

---
+++ src/main/java/com/dl/base/util/OSSPicUtils.java
@@ -0,0 +1,23 @@
+package com.dl.base.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class OSSPicUtils {
+	
+	/**
+	 * 获取图片链接
+	 * @param width
+	 * @param height
+	 * @param picUrl
+	 * @return
+	 */
+	public static final String getPicFormat(int width,int height,String picUrl) {
+		StringBuilder builder = new StringBuilder();
+		if(!StringUtils.isEmpty(picUrl)) {
+			builder.append(picUrl);
+			//将图按比例缩略到原来的 1/3
+			builder.append("?x-oss-process=image/resize,p_30");
+		}
+		return builder.toString();
+	}
+}
