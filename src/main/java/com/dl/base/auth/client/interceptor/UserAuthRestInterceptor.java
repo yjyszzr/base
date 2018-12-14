9c5a125f1f7ec57c1eff99fd65112134881e7030
wenhuaiteng <wenhuaiteng@126.com>
2018-12-14 19:42:50 +0800

    log日志...

--- src/main/java/com/dl/base/auth/client/interceptor/UserAuthRestInterceptor.java
+++ src/main/java/com/dl/base/auth/client/interceptor/UserAuthRestInterceptor.java
@@ -42,7 +42,9 @@ public class UserAuthRestInterceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         try {
         	String token = null;
+    		log.info("[preHandle]" + " request:" + request + " userAuthConfig:" + userAuthConfig);
         	if(request != null && userAuthConfig != null) {
+        		log.info("[preHandle]" + " tokenHeader:" + userAuthConfig.getTokenHeader());
         		token = request.getHeader(userAuthConfig.getTokenHeader());	
         	}
         	log.info("[preHandle]" + " token:" + token);
