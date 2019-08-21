9b417a5b4535444ae5788d491825f2ec0a843893
lifeilong <lifeilong@caixiaomi.com>
2019-08-21 13:22:23 +0800

    xg

--- src/main/java/com/dl/base/util/MD5Util.java
+++ src/main/java/com/dl/base/util/MD5Util.java
@@ -1,10 +1,51 @@
 package com.dl.base.util;
 
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MD5Util {
-
+	
+	/**
+     * Encodes a string
+     * 
+     * @param str String to encode
+     * @return Encoded String
+     * @throws Exception
+     */
+    public static String cryptForUTF(String str)
+    {
+        if (str == null || str.length() == 0) {
+            throw new IllegalArgumentException("String to encript cannot be null or zero length");
+        }
+        
+        StringBuffer hexString = new StringBuffer();
+        
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            try {
+				md.update(str.getBytes("UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            byte[] hash = md.digest();
+            
+            for (int i = 0; i < hash.length; i++) {
+                if ((0xff & hash[i]) < 0x10) {
+                    hexString.append("0" + Integer.toHexString((0xFF & hash[i])));
+                }               
+                else {
+                    hexString.append(Integer.toHexString(0xFF & hash[i]));
+                }               
+            }
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        
+        return hexString.toString();
+    }
 	/**
      * Encodes a string
      * 
